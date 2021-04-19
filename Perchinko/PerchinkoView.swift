6a8f2a0bb945f03f4939da326d56aa7e10bea493
Steve Sparks <sparks@bignerdranch.com>
2021-04-19 12:42:46 -0400

    Adds images

--- Perchinko/PerchinkoView.swift
+++ Perchinko/PerchinkoView.swift
@@ -6,8 +6,29 @@
 //
 
 import UIKit
+import AVFoundation
+
+class BoundaryIdentifier: NSObject, NSCopying {
+    var name: String = "boundary"
+    var value: Int = 1
+    
+    init(_ name: String, _ value: Int) {
+        self.name = name
+        self.value = value
+    }
+    
+    func copy(with zone: NSZone? = nil) -> Any {
+        return BoundaryIdentifier(name, value)
+    }
+}
 
 class PerchinkoView: UIView {
+    var score = 0 {
+        didSet {
+            scoreLabel.text = "Score: \(score)"
+        }
+    }
+    
     var _animator: UIDynamicAnimator?
     var animator: UIDynamicAnimator {
         set {
@@ -24,8 +45,11 @@ class PerchinkoView: UIView {
         }
     }
     
+    let scoreLabel = UILabel()
+    
     let gravity = UIGravityBehavior()
     let collider = UICollisionBehavior()
+    let pointCounter = UICollisionBehavior()
     
     let pinBehavior: UIDynamicItemBehavior = {
         let beh = UIDynamicItemBehavior()
@@ -54,8 +78,20 @@ class PerchinkoView: UIView {
         for v in subviews {
             v.removeFromSuperview()
         }
+        pointCounter.collisionDelegate = self
+        pointCounter.collisionMode = .boundaries
+        animator.addBehavior(pointCounter)
+        
         balls.removeAll()
         pins.removeAll()
+        
+        addSubview(scoreLabel)
+        scoreLabel.translatesAutoresizingMaskIntoConstraints = false
+        scoreLabel.leftAnchor.constraint(equalTo: leftAnchor).isActive = true
+        scoreLabel.topAnchor.constraint(equalTo: topAnchor).isActive = true
+        scoreLabel.textColor = .black
+        scoreLabel.font = .boldSystemFont(ofSize: 14.0)
+        score = 0
     }
     
     override func didMoveToSuperview() {
@@ -84,32 +120,56 @@ class PerchinkoView: UIView {
         
         addPins()
         addBalls()
+        addBars()
     }
     
+    func addBars() {
+        let offsets = oddRowOffsets
+        let y = CGFloat(435.0)
+
+        
+        let left = CGPoint(x: offsets[2], y: y)
+        let center1 = CGPoint(x: offsets[3], y: y)
+        let center2 = CGPoint(x: offsets[4], y: y)
+        let right = CGPoint(x: offsets[5], y: y)
+
+        
+        pointCounter.addBoundary(withIdentifier: BoundaryIdentifier("left", 1), from: left, to: center1)
+        pointCounter.addBoundary(withIdentifier: BoundaryIdentifier("center", 2), from: center1, to: center2)
+        pointCounter.addBoundary(withIdentifier: BoundaryIdentifier("right", 1), from: center2, to: right)
+    }
+
+    var oddRowOffsets: [CGFloat] {
+        let width = max(bounds.size.width, 10.0)
+        let xWidth = (width / 10.0)
+        let half = xWidth / 2.0
+        return Array(1...8).map { half + CGFloat($0) * xWidth }
+    }
+
+    var evenRowOffsets: [CGFloat] {
+        let width = max(bounds.size.width, 10.0)
+        let xWidth = (width / 10.0)
+
+        return Array(1...9).map { CGFloat($0) * xWidth }
+    }
+
     func addPins(_ numberOfPins: Int = 30) {
         for pin in pins {
             pin.removeFromSuperview()
         }
         pins = []
-
-        // odd rows
-
-        let width = max(bounds.size.width, 10.0)
-        let xWidth = (width / 10.0)
         
-        for y in [20, 40, 60, 80, 100, 120] {
-            for x in 2...8 {
-                let point = CGPoint(x: CGFloat(x) * xWidth, y: 30.0 + CGFloat(y) * 3.5)
-                let pin = addPinAt(point)
-                pin.tag = x
+        for y in [20, 40, 60, 80, 100] {
+            for x in evenRowOffsets {
+                let point = CGPoint(x: x, y: 30.0 + CGFloat(y) * 3.5)
+                addPinAt(point)
             }
         }
 
-        for y in [30, 50, 70, 90, 110, 130] {
-            for x in 1...8 {
-                let point = CGPoint(x: (xWidth/2) + CGFloat(x) * xWidth, y: 30.0 + CGFloat(y) * 3.5)
-                let pin = addPinAt(point)
-                pin.tag = x
+        for y in [30, 50, 70, 90, 110] {
+            for x in oddRowOffsets {
+                let point = CGPoint(x: x, y: 30.0 + CGFloat(y) * 3.5)
+                addPinAt(point)
             }
         }
     }
@@ -125,7 +185,7 @@ class PerchinkoView: UIView {
         balls.append(view)
         gravity.addItem(view)
         collider.addItem(view)
-        
+        pointCounter.addItem(view)
         
         return view
     }
@@ -150,9 +210,11 @@ class PerchinkoView: UIView {
         }
         balls = []
 
+        let width = self.bounds.size.width
+
         for x in 1...numberOfBalls {
             DispatchQueue.main.asyncAfter(deadline: .now() + (Double(x) * 0.1)) {
-                let rX = fmod(CGFloat(arc4random()), max(self.bounds.size.width, 4.0))
+                let rX = width / 4 + fmod(CGFloat(arc4random()), max(width / 2, 4.0))
                 let rY = fmod(CGFloat(arc4random()), min(max(self.bounds.size.height, 4.0), 100))
                 let ball = self.addBallAt(CGPoint(x: rX, y: rY))
                 ball.tag = x
@@ -161,7 +223,13 @@ class PerchinkoView: UIView {
     }
     
     @objc @IBAction func reset(_ sender: Any?) {
-        balls.forEach { $0.removeFromSuperview() }
+        balls.forEach {
+            collider.removeItem($0)
+            pointCounter.removeItem($0)
+            ballBehavior.removeItem($0)
+            gravity.removeItem($0)
+            $0.removeFromSuperview()
+        }
         balls = []
         
         addBalls()
@@ -182,13 +250,53 @@ class PinView: UIView {
     }
 }
 
-class BallView: UIImageView {
+
+class HoleView: UIView {
+    var value = 1
     
+    override var collisionBoundingPath: UIBezierPath {
+        return UIBezierPath(cgPath: CGPath(ellipseIn: self.bounds, transform: nil))
+    }
+}
+
+class BallView: UIImageView {
     override func willMove(toWindow newWindow: UIWindow?) {
         super.willMove(toWindow: newWindow)
         image = UIImage(named: "pinball")
     }
+    
     override var collisionBoundingPath: UIBezierPath {
         return UIBezierPath(cgPath: CGPath(ellipseIn: self.bounds, transform: nil))
+    }
+}
+
+extension PerchinkoView: UICollisionBehaviorDelegate {
+    func collisionBehavior(_ behavior: UICollisionBehavior, beganContactFor item: UIDynamicItem, withBoundaryIdentifier identifier: NSCopying?, at p: CGPoint) {
+        guard let ball = item as? BallView else {
+            print("bold")
+            return
+        }
+        guard let ident = identifier as? BoundaryIdentifier else {
+            print("weird")
+            return
+        }
+        self.collider.removeItem(ball)
+        self.pointCounter.removeItem(ball)
+        self.gravity.removeItem(ball)
+        self.ballBehavior.removeItem(ball)
+
+        score += ident.value
+
+        if let idx = balls.firstIndex(of: ball) {
+            balls.remove(at: idx)
+        }
+        
+//        AudioServicesPlaySystemSound(1016)
+        ball.transform = .identity
+        UIView.animate(withDuration: 0.5, animations: {
+            ball.transform = CGAffineTransform(scaleX: 0.001, y: 0.001)
+        }, completion: { _ in
+            ball.removeFromSuperview()
+        })
     }
 }
